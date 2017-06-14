7d9de8cf920d1aadb56ab2dec85659ea69b2e775
Jean <jean.gabriel.leroux@gmail.com>
2017-06-14 15:49:49 +0200

    USGDA009 -planning des absences#12 - Affichage Angular v2

---
+++ front/app/planning/planning.component.js
@@ -0,0 +1,14 @@
+import './planning.component.css'
+import template from './planning.component.html'
+
+class controller {
+  constructor () {
+    this.titre = 'Planning'
+  }
+}
+
+export let PlanningComponent = {
+  template,
+  controller,
+  bindings: {}
+}
