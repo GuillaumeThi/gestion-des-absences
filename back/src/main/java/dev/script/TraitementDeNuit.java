b7c459cf317bcb0b6671afb1d681c36e39bbc42d
Franc LAVAUD <franc.lavaud@gmail.com>
2017-06-14 18:15:09 +0200

    USGDA017 - Traitement de nuit #20 - Changement de technologie (->spring annotation), test recuperation donnees ok

---
+++ back/src/main/java/dev/script/TraitementDeNuit.java
@@ -0,0 +1,32 @@
+package dev.script;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import dev.entity.Absence;
+import dev.entity.Statut;
+import dev.repository.AbsenceRepository;
+
+@Component
+public class TraitementDeNuit {
+
+@Autowired AbsenceRepository absRepository;
+	
+	private static final Logger log = LoggerFactory.getLogger(TraitementDeNuit.class);
+
+    @Scheduled(cron = "0/5 * * * * ?")
+    public void fetchData() {
+    	List<Absence> listAbs =  absRepository.findByStatut(Statut.INITIALE);
+		listAbs.isEmpty();
+		for(Absence a: listAbs){
+			log.info("hello "+ a.getStatut().toString());
+		}
+        
+    }
+
+}
