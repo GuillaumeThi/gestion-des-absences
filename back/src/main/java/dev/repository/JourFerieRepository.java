142c9be34545f4f57a21d806ce2576fbabc01b0c
Jean <jean.gabriel.leroux@gmail.com>
2017-06-15 09:45:39 +0200

    USGDA009 - planning des absences#12 - debut back

---
+++ back/src/main/java/dev/repository/JourFerieRepository.java
@@ -0,0 +1,12 @@
+package dev.repository;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import dev.entity.JourFerie;
+
+
+public interface JourFerieRepository extends JpaRepository<JourFerie, Integer>{
+}
