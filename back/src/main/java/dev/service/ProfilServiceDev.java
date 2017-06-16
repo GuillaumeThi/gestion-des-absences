508bb6a2b60e0409a67175319f806457b404110c
FrLava <franc.lavaud@gmail.com>
2017-06-16 17:47:02 +0200

    Update ProfilServiceDev.java

--- back/src/main/java/dev/service/ProfilServiceDev.java
+++ back/src/main/java/dev/service/ProfilServiceDev.java
@@ -15,36 +15,22 @@ import dev.entity.TypeAbsence;
 import dev.entity.TypeJourFerie;
 import dev.entity.Utilisateur;
 import dev.repository.AbsenceRepository;
-<<<<<<< HEAD
-=======
 import dev.repository.JourFerieRepository;
->>>>>>> USGDA017 - Traitement de nuit #20 - US en review - correction
 import dev.repository.UtilisateurRepository;
 
 @Service
 public class ProfilServiceDev {
 
-<<<<<<< HEAD
-	@Autowired CollaborateurService collabServ;
-	@Autowired UtilisateurRepository utilisateurRepo;
-	@Autowired AbsenceRepository absenceRepo;
-=======
 	@Autowired CollaborateurService collabserv;
 	@Autowired UtilisateurRepository utilisateurrepo;
 	@Autowired AbsenceRepository absR;
 	@Autowired JourFerieRepository jFR;
->>>>>>> USGDA017 - Traitement de nuit #20 - US en review - correction
-	
+
 	public void init() {
 
-<<<<<<< HEAD
-		absenceRepo.deleteAll();
-		utilisateurRepo.deleteAll();
-=======
 		absR.deleteAll();
 		utilisateurrepo.deleteAll();
 		jFR.deleteAll();
->>>>>>> USGDA017 - Traitement de nuit #20 - US en review - correction
 
 		List<Collaborateur> listcollab = collabServ.listerCollaborateurs();
 		for (Collaborateur collaborateur : listcollab) {
@@ -53,13 +39,10 @@ public class ProfilServiceDev {
 			}else if (collaborateur == listcollab.get(1)){
 				utilisateurRepo.save(new Utilisateur(collaborateur.getMatricule(), null, Role.MANAGER));
 			}else {
-<<<<<<< HEAD
-				utilisateurRepo.save(new Utilisateur(collaborateur.getMatricule(), null, Role.COLLABORATEUR));
-=======
+
 				Utilisateur user = new Utilisateur(collaborateur.getMatricule(), null, Role.COLLABORATEUR);
 				utilisateurrepo.save(user);
 				absR.saveAndFlush(new Absence( LocalDate.parse("2017-06-20"), LocalDate.parse("2017-06-21"), TypeAbsence.getRandomTypeAbsence(), Statut.INITIALE, "",user));
->>>>>>> USGDA017 - Traitement de nuit #20 - US en review - correction
 			}
 			//return listcollab;
 		}
