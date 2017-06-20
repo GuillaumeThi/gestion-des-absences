e4c257904b21937f09d0affc7000953ec796b4f0
Guillaume THIRIET <fake@fake.com>
2017-06-20 12:54:36 +0200

    USGDA005 #8

--- back/src/main/java/dev/controller/AbsenceController.java
+++ back/src/main/java/dev/controller/AbsenceController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import dev.entity.Absence;
 import dev.entity.TypeAbsence;
 import dev.repository.AbsenceRepository;
 import dev.service.AbsenceService;
