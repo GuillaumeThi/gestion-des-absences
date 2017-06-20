3dc45f9e3ff744650cc402555c1cb94f4c519cef
GuillaumeThi <guillaume-thiriet@live.fr>
2017-06-20 13:14:18 +0200

    Update absence.service.js

--- front/app/service/absence.service.js
+++ front/app/service/absence.service.js
@@ -1,43 +1,42 @@
 export class AbsenceService {
-  constructor ($http, API_URL, publicPath, LoginService) {
-    this.$http = $http
-    this.apiUrl = API_URL + publicPath + 'absences'
-    this.loginService = LoginService
+	constructor ($http, API_URL, publicPath, LoginService) {
+		this.$http = $http
+		this.apiUrl = API_URL + publicPath + 'absences'
+		this.loginService = LoginService
 
-    this.user = this.loginService.loadCookies()
-  }
+		this.user = this.loginService.loadCookies()
+	}
 
-  listerAbsencesUtilisateurCourant () {
-    let absences = this.$http.get(this.apiUrl + '?matricule=' + this.user.matriculeCollab)
-      .then(response => {
-        return response.data.absences
-.filter(absence => absence.utilisateur.id === this.loginService.getId(this.user))
-/* .map(absence => {
-absence.dateDebut = toLongFrenchFormat(absence.dateDebut)
-absence.dateFin = toLongFrenchFormat(absence.dateFin)
-return absence
-}) */
-      })
+	listerAbsencesUtilisateurCourant () {
 
-    return absences
-  }
+		let absences = this.$http.get(this.apiUrl + "?matricule=" + this.user.matriculeCollab)
+			.then(response => response.data.absences.filter(absence => absence.utilisateur.id === this.loginService.getId(this.user)))
 
-  getCompteurCongesPayes () {
-    return this.$http.get(this.apiUrl + '?matricule=' + this.user.matriculeCollab)
-.then(response => response.data.congesPayes)
-  }
+		return absences
+	}
 
-  getCompteurRTT () {
-    return this.$http.get(this.apiUrl + '?matricule=' + this.user.matriculeCollab)
-.then(response => response.data.RTT)
-  }
+	getCompteurCongesPayes () {
 
-  listerTypesAbsence () {
-    return this.$http.get(this.apiUrl + '/nouvelle-demande')
-      .then(response => response.data)
-  }
+		return this.$http.get(this.apiUrl + "?matricule=" + this.user.matriculeCollab)
+			.then(response => response.data.congesPayes)
+	}
 
-  parser (date) {
-    return date.dayOfMonth + ' ' + date.month + ' ' + date.year
-  }
+	getCompteurRTT () {
+
+		return this.$http.get(this.apiUrl + "?matricule=" + this.user.matriculeCollab)
+			.then(response => response.data.RTT)
+	}
+
+	listerTypesAbsence () {
+
+		return this.$http.get(this.apiUrl + '/nouvelle-demande')
+			.then(response => response.data)
+	}
+
+	parser (date) {
+
+		let moisFrancais = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"]
+		
+		return date.dayOfMonth + ' ' + moisFrancais[date.monthValue] + ' ' + date.year
+	}
 }
