3768382bb0dcb9ab4adbf6deb7641e3b7bf83ce7
GuillaumeThi <guillaume-thiriet@live.fr>
2017-06-20 13:16:31 +0200

    Update lister-absences.component.js

--- front/app/absence/lister-absences.component.js
+++ front/app/absence/lister-absences.component.js
@@ -2,36 +2,36 @@ import template from './lister-absences.component.html'
 import './lister-absences.component.css'
 
 class controller {
-  constructor (AbsenceService, $location) {
-    this.AbsenceService = AbsenceService
-    this.$location = $location
-  }
+    constructor (AbsenceService, $location) {
+        this.AbsenceService = AbsenceService
+        this.$location = $location
+    }
 
-  $onInit () {
-    this.AbsenceService.listerAbsencesUtilisateurCourant()
-        .then(absences => {
-          this.absences = absences
-          this.absences.forEach(absence => {
-            absence.dateDebut = this.AbsenceService.parser(absence.dateDebut)
-            absence.dateFin = this.AbsenceService.parser(absence.dateFin)
-          })
-          return this.absences
-        })
+    $onInit () {
+        this.AbsenceService.listerAbsencesUtilisateurCourant()
+            .then(absences => { 
+                absences.forEach(absence => {
+                    absence.dateDebut = this.AbsenceService.parser(absence.dateDebut)
+                    absence.dateFin = this.AbsenceService.parser(absence.dateFin)
+                })
 
-    this.AbsenceService.getCompteurCongesPayes()
-        .then(congesPayes => this.congesPayes = congesPayes)
+                return this.absences = absences
+            })
 
-    this.AbsenceService.getCompteurRTT()
-        .then(RTT => this.RTT = RTT)
-  }
+        this.AbsenceService.getCompteurCongesPayes()
+            .then(congesPayes => this.congesPayes = congesPayes)
+
+        this.AbsenceService.getCompteurRTT()
+            .then(RTT => this.RTT = RTT)
+    }
 }
 
 export let ListerAbsencesComponent = {
-  controller,
-  template,
-  bindings: {
-    absences: '<',
-    congesPayes: '<',
-    RTT: '<'
-  }
+    controller,
+    template,
+    bindings: {
+        absences: '<',
+        congesPayes: '<',
+        RTT: '<'
+    }
 }
